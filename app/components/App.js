0ce280bed821e1b2bd19f5176cf5ced5bc417592
HCI01\meads <meads@hcpci.com>
2016-09-06 12:14:08 -0400

    Step 10. Socket.IO - Real-time User Count

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Footer from './Footer';
+import Navbar from './Navbar';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        {this.props.children}
-      </div>
+  <Navbar history={this.props.history} />
+  {this.props.children}
+  <Footer />
+</div>
     );
   }
 }
